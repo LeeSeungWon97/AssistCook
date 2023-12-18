44faae95aa64d8c41484a25405992b282dfb85b6
LeeSeungWon97 <tmddnjs1122@naver.com>
2023-12-18 15:27:27 +0900

    프로젝트 생성
    AssistCook 프로젝트 생성

---
+++ AssistCook/src/main/java/com/toy/AssistCook/AssistCookApplication.java
@@ -0,0 +1,13 @@
+package com.toy.AssistCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistCookApplication.class, args);
+	}
+
+}
---
+++ AssistCook/src/test/java/com/toy/AssistCook/AssistCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.AssistCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
