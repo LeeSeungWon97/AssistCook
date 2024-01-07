252152b49fbd01d13143a3510071795c871aea30
LeeSeungWon97 <tmddnjs1122@naver.com>
2024-01-07 15:40:12 +0900

    회원가입 검증 기능 추가
    validation 라이브러리를 사용하여 회원가입 시 검증처리를 수행

--- AssistCook/src/main/java/com/toy/AssistCook/controller/MemberController.java
+++ AssistCook/src/main/java/com/toy/AssistCook/controller/MemberController.java
@@ -2,13 +2,17 @@ package com.toy.AssistCook.controller;
 
 import com.toy.AssistCook.domain.member.MemberForm;
 import com.toy.AssistCook.service.member.MemberServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Controller
@@ -19,14 +23,23 @@ public class MemberController {
     private final MemberServiceImpl memberService;
 
     @GetMapping("new")
-    public String joinForm() {
+    public String joinForm(Model model) {
         log.info("회원 가입 페이지 이동 요청");
+        if (!model.containsAttribute("memberForm")) {
+            model.addAttribute("memberForm", new MemberForm());
+        }
         return "members/joinForm";
     }
 
     @PostMapping("new")
-    public String joinMember(@ModelAttribute MemberForm memberForm) {
+    public String joinMember(@Valid @ModelAttribute MemberForm memberForm, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         log.info("요청 데이터={}", memberForm);
+        if (bindingResult.hasErrors()) {
+            log.info("error={}", bindingResult);
+            redirectAttributes.addFlashAttribute("memberForm", memberForm);
+            redirectAttributes.addFlashAttribute("error", "회원가입에 실패했습니다. 입력 정보를 확인하고 다시 요청해주십시오.");
+            return "redirect:/members/new";
+        }
         memberService.save(memberForm);
         return "redirect:/";
     }
--- AssistCook/src/main/java/com/toy/AssistCook/domain/member/MemberForm.java
+++ AssistCook/src/main/java/com/toy/AssistCook/domain/member/MemberForm.java
@@ -1,19 +1,32 @@
 package com.toy.AssistCook.domain.member;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
 @Data
 public class MemberForm {
     private Long id;
+    @NotBlank(message = "아이디를 입력해주세요")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]*$",message = "아이디는 영문 또는 영문과 숫자의 조합이어야 합니다.")
     private String loginId;
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    @Size(min = 8, max = 15, message = "비밀번호는 8~15글자이어야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z0-9!@#$%^&*()_+{}\\[\\]:;<>,.?~\\\\/-])",message = "비밀번호는 영문,숫자,특수문자의 조합이어야 합니다.")
     private String pw;
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
+    @NotBlank(message = "생년월일을 입력해주세요")
     private String birth;
     private String postcode;
     private String address;
+    @NotBlank(message = "상세 주소를 입력해주세요")
     private String detailAddress;
     private String extraAddress;
+    @NotBlank(message = "이메일을 입력해주세요")
     private String email;
+    @NotBlank(message = "도메인을 입력해주세요")
     private String domain;
 }
